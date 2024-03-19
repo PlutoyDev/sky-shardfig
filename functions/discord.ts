b0823c3cf85e5dda9e3e61ae6f597bdaa57e4cd4
PlutoyDev <plutoy.dev@outlook.com>
2024-03-19 08:36:14 +0800

    remove shared imports

--- functions/discord.ts
+++ functions/discord.ts
@@ -10,7 +10,35 @@ import {
   InteractionResponseType,
 } from 'discord-api-types/v10';
 import nacl from 'tweetnacl';
-import { GlobalShardConfig } from '../shared/types';
+
+const memories = [
+  'jellyfish',
+  'crab',
+  'manta',
+  'krill',
+  'whale',
+  'elder',
+] as const;
+
+interface DailyShardConfig {
+  memory?: (typeof memories)[number];
+  variation?: string;
+  isBugged?: boolean;
+  bugType?: 'noShard' | 'noMemory';
+  isDisabled?: boolean;
+  disabledReason?: string;
+  credits?: string[];
+  lastModified?: string;
+  lastModifiedBy?: string;
+}
+
+interface GlobalShardConfig {
+  dailyMap: Record<string, DailyShardConfig>; //key = yyyy-mm-dd
+  isBugged?: boolean;
+  bugType?: 'inaccurate' | 'tgc :/';
+  lastModified?: string;
+  lastModifiedBy?: string;
+}
 
 interface RequiredEnv {
   UPSTASH_REDIS_REST_URL: string;
