89c6ba8c6deddc4c0beae580d72f0ae5b99e176b
PlutoyDev <plutoy.dev@outlook.com>
2024-03-26 12:42:37 +0800

    revamping the data structure of remote config

--- shared/lib.ts
+++ shared/lib.ts
@@ -47,134 +47,198 @@ export function getShardMapInfo(date: DateTime) {
   const numVariants = numMapVarients[map as keyof typeof numMapVarients] ?? 1;
   return { map, numVariants };
 }
-export interface DailyShardConfig {
+
+export interface DailyConfig {
   memory?: number;
+  memoryBy?: string;
   variation?: number;
-  isBugged?: boolean;
-  bugType?: 'noShard' | 'noMemory';
-  isDisabled?: boolean;
-  disabledReason?: string;
-  credits?: string[];
-  lastModified?: string;
-  lastModifiedBy?: string;
-}
-
-export interface DailyShardConfigStringified {
-  memory?: string;
-  variation?: string;
-  isBugged?: string;
-  bugType?: string;
-  isDisabled?: string;
-  disabledReason?: string;
-  credits?: string;
-  lastModified?: string;
-  lastModifiedBy?: string;
-}
-
-export function parseDailyConfig(
-  dailyShardConfig: DailyShardConfigStringified
-): DailyShardConfig {
-  return {
-    memory: dailyShardConfig.memory
-      ? parseInt(dailyShardConfig.memory)
-      : undefined,
-    variation: dailyShardConfig.variation
-      ? parseInt(dailyShardConfig.variation)
-      : undefined,
-    isBugged: dailyShardConfig.isBugged === 'true',
-    bugType: dailyShardConfig.bugType as 'noShard' | 'noMemory' | undefined,
-    isDisabled: dailyShardConfig.isDisabled === 'true',
-    disabledReason: dailyShardConfig.disabledReason,
-    credits: dailyShardConfig.credits
-      ? dailyShardConfig.credits.split(',')
-      : [],
-    lastModified: dailyShardConfig.lastModified,
-    lastModifiedBy: dailyShardConfig.lastModifiedBy,
+  variationBy?: string;
+  override?: {
+    hasShard?: boolean;
+    isRed?: boolean;
+    realm?: number;
+    map?: string;
+    offset?: number;
   };
+  overrideBy?: string;
+  overrideReason?: string;
+  version?: number;
+  lastModified?: DateTime;
+}
+
+export interface DailyConfigFromRedis {
+  memory?: string;
+  memoryBy?: string;
+  variation?: string;
+  variationBy?: string;
+  override?: string;
+  overrideBy?: string;
+  overrideReason?: string;
+  version?: string;
+  lastModified?: string;
+}
+
+export async function getDailyConfig<
+  Keys extends (keyof DailyConfig)[] = [
+    'memory',
+    'memoryBy',
+    'variation',
+    'variationBy',
+    'override',
+    'overrideBy',
+    'overrideReason',
+    'lastModified'
+  ]
+>(
+  redis: Redis,
+  date: DateTime,
+  keys?: Keys
+): Promise<
+  Pick<DailyConfigFromRedis, Keys[number]> | DailyConfigFromRedis | undefined
+> {
+  const isoDate = date.toISODate();
+  if (!isoDate) return undefined;
+  const hashKey = `daily:${isoDate}`;
+  if (!keys) {
+    return (await redis.hgetall(hashKey)) as DailyConfigFromRedis;
+  }
+
+  return (await redis.hmget(hashKey, ...keys)) as Pick<
+    DailyConfigFromRedis,
+    Keys[number]
+  >;
+}
+
+export async function getParsedDailyConfig<Keys extends (keyof DailyConfig)[]>(
+  redis: Redis,
+  date: DateTime,
+  keys?: Keys
+) {
+  const config = (await getDailyConfig(redis, date, keys)) as
+    | DailyConfigFromRedis
+    | undefined;
+  if (!config) return undefined;
+
+  const parsedConfig: DailyConfig = {};
+  if (config.memory) parsedConfig.memory = parseInt(config.memory);
+  if (config.memoryBy) parsedConfig.memoryBy = config.memoryBy;
+  if (config.variation) parsedConfig.variation = parseInt(config.variation);
+  if (config.variationBy) parsedConfig.variationBy = config.variationBy;
+  if (config.override) parsedConfig.override = JSON.parse(config.override);
+  if (config.overrideBy) parsedConfig.overrideBy = config.overrideBy;
+  if (config.overrideReason)
+    parsedConfig.overrideReason = config.overrideReason;
+  if (config.version) parsedConfig.version = parseInt(config.version);
+  if (config.lastModified)
+    parsedConfig.lastModified = DateTime.fromISO(config.lastModified);
+
+  if (!keys) return parsedConfig as DailyConfig;
+  return parsedConfig as Pick<DailyConfig, Keys[number]>;
+}
+
+export async function setDailyConfig(
+  redis: Redis,
+  date: DateTime,
+  config: Omit<
+    DailyConfig,
+    | 'lastModified'
+    | 'version'
+    | 'authorMap'
+    | 'memoryBy'
+    | 'variationBy'
+    | 'overrideBy'
+  >,
+  authorId: string
+) {
+  const isoDate = date.toISODate();
+  if (!isoDate) throw new Error('Invalid date');
+  if (!config.memory && !config.variation && !config.override)
+    throw new Error('No config to set');
+
+  // TODO: Add action log
+
+  const configStringified: DailyConfigFromRedis = {};
+  if (config.memory) {
+    configStringified.memory = config.memory.toString();
+    configStringified.memoryBy = authorId;
+  }
+  if (config.variation) {
+    configStringified.variation = config.variation.toString();
+    configStringified.variationBy = authorId;
+  }
+  if (config.override) {
+    if (!config.overrideReason) throw new Error('Missing override reason');
+    configStringified.override = JSON.stringify(config.override);
+    configStringified.overrideBy = authorId;
+    configStringified.overrideReason = config.overrideReason;
+  }
+
+  configStringified.lastModified = DateTime.now().toISO();
+
+  await Promise.all([
+    redis.hmset(
+      `daily:${isoDate}`,
+      configStringified as Record<string, string>
+    ),
+    redis.sadd('edited_dates', isoDate),
+  ]);
+
+  await redis.hincrby(`daily:${isoDate}`, 'version', 1);
 }
 
 export interface GlobalShardConfig {
-  dailyMap: Record<string, DailyShardConfig>; //key = yyyy-mm-dd
-  isBugged?: boolean;
-  bugType?: 'inaccurate' | 'tgc :/';
-  lastModified?: string;
-  lastModifiedBy?: string;
+  // This controls the global state of the application
+  bugged?: boolean;
+  buggedReason?: string;
+  buggedReasonKey?: string;
 }
 
-export async function getGlobalShardConfig(
-  redis: Redis
-): Promise<GlobalShardConfig> {
-  const res = await redis.mget<(string | null)[]>(
-    'globalIsBugged',
-    'globalBugType',
-    'globalLastModified',
-    'globalLastModifiedBy'
+export async function getGlobalShardConfig(redis: Redis) {
+  const config = await redis.hgetall<Record<keyof GlobalShardConfig, string>>(
+    'global'
   );
-
-  const [isBugged, bugType, lastModified, lastModifiedBy] = res;
-
-  const globalShardConfig: GlobalShardConfig = { dailyMap: {} };
-
-  if (isBugged) {
-    globalShardConfig.isBugged = isBugged === 'true';
+  if (!config) return {};
+  const parsedConfig: GlobalShardConfig = {};
+  if (config.bugged && config.bugged === 'true') {
+    parsedConfig.bugged = true;
+    parsedConfig.buggedReason = config.buggedReason;
+    parsedConfig.buggedReasonKey = config.buggedReasonKey;
   }
-
-  if (bugType) {
-    globalShardConfig.bugType = bugType as 'inaccurate' | 'tgc :/';
-  }
-
-  if (lastModified) {
-    globalShardConfig.lastModified = lastModified;
-  }
-
-  if (lastModifiedBy) {
-    globalShardConfig.lastModifiedBy = lastModifiedBy;
-  }
-
-  return globalShardConfig;
 }
 
-export async function getDailyShardConfig(
+export async function setGlobalShardConfig(
   redis: Redis,
-  date?: DateTime
-): Promise<[string, DailyShardConfigStringified] | undefined> {
-  const isoDate =
-    date?.toISODate() ??
-    DateTime.now().setZone('America/Los_Angeles').toISODate();
-  if (!isoDate) return undefined;
+  data: GlobalShardConfig
+) {
+  const configStringified: Record<string, string> = {};
+  if (data.bugged) {
+    configStringified.bugged = 'true';
+    configStringified.buggedReason = data.buggedReason ?? '';
+    configStringified.buggedReasonKey = data.buggedReasonKey ?? '';
+  } else {
+    configStringified.bugged = 'false';
+    await redis.hdel('global', 'buggedReason', 'buggedReasonKey');
+  }
 
-  const config = await redis.hgetall(`daily:${isoDate}`);
-
-  if (config === null) return undefined;
-  return [isoDate, config as DailyShardConfigStringified];
+  await redis.hmset('global', configStringified);
 }
 
-export async function getParsedDailyShardConfig<
-  Keys extends (keyof DailyShardConfig)[]
->(
+export async function pushAuthorName(
   redis: Redis,
-  keys: Keys,
-  date?: DateTime
-): Promise<Pick<DailyShardConfig, Keys[number]>> {
-  date ??= DateTime.now().setZone('America/Los_Angeles');
+  authorId: string,
+  authorName: string
+) {
+  await redis.hset('author_names', { [authorId]: authorName });
+}
 
-  const isoDate = date.toISODate();
-  if (!isoDate) {
-    return Object.fromEntries(keys.map(key => [key, undefined])) as Pick<
-      DailyShardConfig,
-      Keys[number]
-    >;
-  }
+export async function getAuthorNames(redis: Redis) {
+  const authorNames = await redis.hgetall('author_names');
+  if (!authorNames) return {};
+  return authorNames as Record<string, string>;
+}
 
-  const config = await redis.hmget(`daily:${isoDate}`, ...keys);
-  if (config === null) {
-    return Object.fromEntries(keys.map(key => [key, undefined])) as Pick<
-      DailyShardConfig,
-      Keys[number]
-    >;
-  }
-  return parseDailyConfig(config as DailyShardConfigStringified) as Pick<
-    DailyShardConfig,
-    Keys[number]
-  >;
+export interface RemoteConfigResponse {
+  dailyMap: Record<string, DailyConfig>;
+  authorName: Record<string, string>;
+  global: GlobalShardConfig;
 }
