3eccf899a18bee00b6f38e5bc2a41aa3acb46993
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2024-03-28 10:48:28 +0800

    set_daily command to replace previous commands

--- src/registerCommand.ts
+++ src/registerCommand.ts
@@ -1,116 +1,69 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { REST } from '@discordjs/rest';
 import {
+  RESTPostAPIChatInputApplicationCommandsJSONBody,
+  RESTPutAPIApplicationGuildCommandsJSONBody,
   RESTPutAPIApplicationGuildCommandsResult,
   Routes,
 } from 'discord-api-types/v10';
 import { memories } from '../shared/lib.js';
 
+if (process.env.DISCORD_CLIENT_ID === undefined) {
+  throw new Error('Missing required environment variable: DISCORD_CLIENT_ID');
+}
+
+if (process.env.DISCORD_BOT_TOKEN === undefined) {
+  throw new Error('Missing required environment variable: DISCORD_BOT_TOKEN');
+}
+
 // Update Discord Command
 try {
   // Setup the command in Discord in channel (1219629213238296676)
+
+  const commands: RESTPutAPIApplicationGuildCommandsJSONBody = [];
+
+  commands.push(
+    new SlashCommandBuilder()
+      .setName('set_daily')
+      .setDescription('Set the daily config')
+      .addIntegerOption(option =>
+        option
+          .setName('memory')
+          .setDescription('Set the memory for the day')
+          .setChoices(...memories.map((m, i) => ({ name: m, value: i })))
+      )
+      .addIntegerOption(option =>
+        option
+          .setName('variation')
+          .setDescription('Set the variation for the day')
+          .setMaxValue(3)
+          .setMinValue(0)
+      )
+      .addStringOption(option =>
+        option
+          .setName('date')
+          .setDescription('Date of the config (YYYY-MM-DD, default today)')
+      )
+      .addBooleanOption(option =>
+        option
+          .setName('has_override')
+          .setDescription('Override the default calculated reason (incomplete)')
+      )
+      .toJSON()
+  );
+
+  // TODO: Add command for global config
+
+  commands.push(
+    new SlashCommandBuilder()
+      .setName('publish')
+      .setDescription('Publish the config to Sky-Shards')
+      .toJSON()
+  );
+
   const rest = new REST({ version: '10' }).setToken(
     process.env.DISCORD_BOT_TOKEN
   );
-
-  const setMemoryCommand = new SlashCommandBuilder()
-    .setName('set_memory')
-    .setDescription('Set the daily memory')
-    .addStringOption(option =>
-      option
-        .setName('memory')
-        .setDescription('The memory for the day')
-        .setRequired(true)
-        .addChoices(
-          ...memories.map(memory => ({ name: memory, value: memory }))
-        )
-    )
-    .addStringOption(option =>
-      option
-        .setName('date')
-        .setDescription(
-          'The date to set the memory for in ISO format (YYYY-MM-DD), defaults to today'
-        )
-    );
-
-  const setVariationCommand = new SlashCommandBuilder()
-    .setName('set_variation')
-    .setDescription('Set the daily variation')
-    .addNumberOption(option =>
-      option
-        .setName('variation')
-        .setDescription('The variation (0, 1, 2, or 3) for the day')
-        .setRequired(true)
-        .setMinValue(0)
-        .setMaxValue(3)
-    )
-    .addStringOption(option =>
-      option
-        .setName('date')
-        .setDescription(
-          'The date to set the variation for in ISO format (YYYY-MM-DD), defaults to today'
-        )
-    );
-
-  const setBuggedStatusCommand = new SlashCommandBuilder()
-    .setName('set_bugged_status')
-    .setDescription('Set the daily bugged status')
-    .addBooleanOption(option =>
-      option
-        .setName('is_bugged')
-        .setDescription('Is the shard bugged?')
-        .setRequired(true)
-    )
-    .addStringOption(option =>
-      option
-        .setName('bug_type')
-        .setDescription('The type of bug')
-        .addChoices(
-          { name: 'No Shard', value: 'noShard' },
-          { name: 'No Memory', value: 'noMemory' }
-        )
-    )
-    .addStringOption(option =>
-      option
-        .setName('date')
-        .setDescription(
-          'The date to set the bugged status for in ISO format (YYYY-MM-DD), defaults to today'
-        )
-    );
-
-  const setDisabledStatusCommand = new SlashCommandBuilder()
-    .setName('set_disabled_status')
-    .setDescription('Set the daily disabled status')
-    .addBooleanOption(option =>
-      option
-        .setName('is_disabled')
-        .setDescription('Is the shard disabled?')
-        .setRequired(true)
-    )
-    .addStringOption(option =>
-      option
-        .setName('reason')
-        .setDescription('The reason for the shard being disabled')
-    )
-    .addStringOption(option =>
-      option
-        .setName('date')
-        .setDescription(
-          'The date to set the disabled status for in ISO format (YYYY-MM-DD), defaults to today'
-        )
-    );
-
-  const publishCommand = new SlashCommandBuilder()
-    .setName('publish')
-    .setDescription('Publish the config to Sky-Shards');
-
-  const commands = [
-    setMemoryCommand,
-    setVariationCommand,
-    setBuggedStatusCommand,
-    setDisabledStatusCommand,
-    publishCommand,
-  ].map(command => command.toJSON());
 
   const guildId = '1219255956207046727';
   const res = (await rest.put(
