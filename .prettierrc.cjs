e136978806c5f3c9a65a706391e28a27f9a625cd
PlutoyDev <plutoy.dev@outlook.com>
2024-04-02 07:32:41 +0800

    install and configure prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,15 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  arrowParens: 'avoid',
+  printWidth: 120,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  quoteProps: 'consistent',
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  importOrder: [
+    '<THIRD_PARTY_MODULES>',
+    '^[./](?!.*\\.(css|scss|less|styl|png|jpg|jpeg|gif|svg|ttf|woff|woff2|eot|otf)$)',
+    '^[./]',
+  ],
+};
