82b8cfb4eac13bad55222154a92b8c7ccf15186f
PlutoyDev <plutoy.dev@outlook.com>
2024-04-03 12:29:37 +0800

    fix override option description

--- src/registerCommand.ts
+++ src/registerCommand.ts
@@ -30,52 +30,36 @@ try {
         option
           .setName('memory')
           .setDescription('Set the memory for the day')
-          .setChoices(...memories.map((m, i) => ({ name: m, value: i })))
+          .setChoices(...memories.map((m, i) => ({ name: m, value: i }))),
       )
       .addIntegerOption(option =>
-        option
-          .setName('variation')
-          .setDescription('Set the variation for the day')
-          .setMaxValue(3)
-          .setMinValue(0)
+        option.setName('variation').setDescription('Set the variation for the day').setMaxValue(3).setMinValue(0),
       )
       .addStringOption(option =>
-        option
-          .setName('date')
-          .setDescription('Date of the config (YYYY-MM-DD, default today)')
+        option.setName('date').setDescription('Date of the config (YYYY-MM-DD, default today)'),
       )
-      .addBooleanOption(option =>
-        option
-          .setName('has_override')
-          .setDescription('Override the default calculated reason (incomplete)')
-      )
-      .toJSON()
+      .addBooleanOption(option => option.setName('has_override').setDescription('Override the default calculation'))
+      .toJSON(),
   );
 
   // TODO: Add command for global config
 
   commands.push(
-    new SlashCommandBuilder()
-      .setName('publish')
-      .setDescription('Publish the config to Sky-Shards')
-      .toJSON()
+    new SlashCommandBuilder().setName('publish').setDescription('Publish the config to Sky-Shards').toJSON(),
   );
 
-  const rest = new REST({ version: '10' }).setToken(
-    process.env.DISCORD_BOT_TOKEN
-  );
+  const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN);
 
   const guildId = '1219255956207046727';
-  const res = (await rest.put(
-    Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, guildId),
-    { body: commands }
-  )) as RESTPutAPIApplicationGuildCommandsResult;
+  const res = (await rest.put(Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, guildId), {
+    body: commands,
+  })) as RESTPutAPIApplicationGuildCommandsResult;
 
   console.log('Successfully registered application commands.');
   console.table(res.map(command => ({ name: command.name, id: command.id })));
 } catch (err) {
   console.error(
     'Failed to update Discord Command',
-    err && typeof err === 'object' && 'message' in err ? err.message : err
+    err && typeof err === 'object' && 'message' in err ? err.message : err,
   );
 }
